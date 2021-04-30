a33d0915ddbcb2db0869c1112aa5bcc7a77dbcb2
LeBulbasaur <antek.zaremba@gmail.com>
2021-04-30 17:30:04 +0200

    server update

--- static/script/checkIfPlaying.js
+++ static/script/checkIfPlaying.js
@@ -8,7 +8,7 @@ if (sessionCookie != "") {
     const ingame = getCookie("ingame")
     if (ingame == "true") {
         const session = getCookie("session")
-        // window.location.href = `/lobby/?valid="${session}"`;
+        window.location.href = `/lobby/?valid="${session}"`;
     }
 } else {
     document.cookie = `ingame=false; expires=${new Date(Date.now() + 1000 * 60 * 60)}`
